f598ceef0bbe0cc3993b71c005fa402995cb578b
Kapitány Erik <kapitanyerik@gmail.com>
2021-04-11 08:48:17 +0200

    Convert MainActivity to be a container of fragments.

--- app/src/main/java/hu/bme/aut/android/myideas/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/android/myideas/MainActivity.kt
@@ -1,11 +1,37 @@
 package hu.bme.aut.android.myideas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(), NavigationHost {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container, TODO("Place here the initial fragment"))
+                .commit()
+        }
+    }
+
+    override fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
+        val transaction = supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, fragment)
+
+        if (addToBackstack) {
+            transaction.addToBackStack(null)
+        }
+
+        transaction.commit()
+    }
+
+    override fun navigateBack() {
+        supportFragmentManager.popBackStack()
     }
 }

1f743647839726ee8b8933a4f3c8fdc498f02232
Kapitány Erik <kapitanyerik@gmail.com>
2021-04-25 17:10:37 +0200

    Replace manually created network idea dto to a generated one.

--- app/src/main/java/hu/bme/aut/android/myideas/models/network/IdeaNetworkEntity.kt
+++
@@ -1,8 +0,0 @@
-package hu.bme.aut.android.myideas.models.network
-
-data class IdeaNetworkEntity(
-    val id: String,
-    val title: String,
-    val description: String,
-    val shortDescription: String
-)

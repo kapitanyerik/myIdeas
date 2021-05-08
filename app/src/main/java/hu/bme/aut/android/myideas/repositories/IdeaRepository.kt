bb6bde96b895d4241b336cce6b5a5b6452520c9d
Kapitány Erik <kapitanyerik@gmail.com>
2021-05-08 11:11:14 +0200

    Modify flows in IdeaRepository to be able to test them.

--- app/src/main/java/hu/bme/aut/android/myideas/repositories/IdeaRepository.kt
+++ app/src/main/java/hu/bme/aut/android/myideas/repositories/IdeaRepository.kt
@@ -8,6 +8,8 @@ import hu.bme.aut.android.myideas.models.mappers.toListOfIdeaDomainModel
 import hu.bme.aut.android.myideas.models.mappers.toNetworkDTO
 import hu.bme.aut.android.myideas.util.DataState
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 
 class IdeaRepository
@@ -21,51 +23,63 @@ constructor(
         emit(DataState.Success(data = "Hello initialized Dashboard"))
     }
 
-    suspend fun createIdea(idea: Idea) = flow {
-        emit(DataState.Loading)
-        try {
+    suspend fun createIdea(idea: Idea): Flow<DataState<Unit>> {
+        return flow {
+            emit(DataState.Loading)
             networkDataSource.createMyIdea(idea.toNetworkDTO())
             cacheDataSource.insert(idea.toCacheDTO())
             emit(DataState.Success(Unit))
-        } catch (e: Exception) {
-            emit(DataState.Error(e.message ?: "Error during new idea creation process"))
         }
+            .catch { throwable ->
+                emit(DataState.Error(throwable.message ?: "Error during new idea creation process"))
+            }
     }
 
-    suspend fun getMyIdeas() = flow {
-        emit(DataState.Loading)
-        try {
+    suspend fun getMyIdeas(): Flow<DataState<Idea>> {
+        return flow {
+            emit(DataState.Loading)
             val myIdeas = networkDataSource.getMyIdeas().toListOfIdeaDomainModel()
             myIdeas.forEach {
                 cacheDataSource.insert(it.toCacheDTO())
                 emit(DataState.Success(it))
             }
-        } catch (e: Exception) {
-            emit(DataState.Error(e.message ?: "Error during getting my ideas process"))
         }
+            .catch { throwable ->
+                emit(DataState.Error(throwable.message ?: "Error during getting my ideas process"))
+            }
     }
 
-    suspend fun updateIdea(idea: Idea) = flow {
-        emit(DataState.Loading)
-        try {
+    suspend fun updateIdea(idea: Idea): Flow<DataState<Unit>> {
+        return flow {
+            emit(DataState.Loading)
             with(idea) {
                 networkDataSource.updateMyIdea(toNetworkDTO())
                 cacheDataSource.insert(toCacheDTO())
-                emit(DataState.Success(this))
+                emit(DataState.Success(Unit))
             }
-        } catch (e: Exception) {
-            emit(DataState.Error(e.message ?: "Error during updating one of my ideas process"))
         }
+            .catch { throwable ->
+                emit(
+                    DataState.Error(
+                        throwable.message ?: "Error during updating one of my ideas process"
+                    )
+                )
+            }
     }
 
-    suspend fun deleteIdea(idea: Idea) = flow {
-        emit(DataState.Loading)
-        try {
+    suspend fun deleteIdea(idea: Idea): Flow<DataState<Unit>> {
+        return flow {
+            emit(DataState.Loading)
             networkDataSource.deleteMyIdea(idea.id)
             cacheDataSource.delete(idea.toCacheDTO())
             emit(DataState.Success(Unit))
-        } catch (e: Exception) {
-            emit(DataState.Error(e.message ?: "Error during deleting one of my ideas process"))
         }
+            .catch { throwable ->
+                emit(
+                    DataState.Error(
+                        throwable.message ?: "Error during deleting one of my ideas process"
+                    )
+                )
+            }
     }
 }
---
+++ app/src/test/java/hu/bme/aut/android/myideas/IdeaRepositoryTest.kt
@@ -0,0 +1,11 @@
+package hu.bme.aut.android.myideas
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class IdeaRepositoryTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
